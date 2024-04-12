f797556731e15d46443d03efa62077fa2ca72e45
zohaibmemon1515 <zohaibmemon1515@gmail.com>
2024-04-12 06:57:38 -0700

    add file

---
+++ come.js
@@ -0,0 +1,4 @@
+var weightInKg = 70;
+var heightInKg = 1.75;
+var bmi = weightInKg / (heightInKg * heightInKg);
+console.log("your BMI is ".concat(bmi));
---
+++ come.ts
@@ -0,0 +1,4 @@
+let weightInKg = 70;
+let heightInKg = 1.75;
+let bmi = weightInKg / (heightInKg * heightInKg)
+console.log(`your BMI is ${bmi}`)//22.857142857142858
