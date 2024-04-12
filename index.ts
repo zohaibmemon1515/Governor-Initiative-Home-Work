06965ec825279eba59be140754bbd514f309cf0d
zohaibmemon1515 <zohaibmemon1515@gmail.com>
2024-04-12 06:35:03 -0700

    add file

---
+++ index.ts
@@ -0,0 +1,48 @@
+import inquirer from "inquirer";
+let myBalance = 50000;
+let myPin = 99220;
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+            name: "pin",
+            message: "Enter your pin",
+            type: "number",
+        }
+    ]
+)  
+if(pinAnswer.pin===myPin){
+    console.log("Correct pin number");
+    let operationAns = await inquirer.prompt(
+        [
+            {
+                name : "operation",
+                message : "Please select option",
+                type : "list",
+                choices : ["withdraw" , "check balance"]
+            }
+        ]
+    );
+    console.log(operationAns);
+if(operationAns.operation==="withdraw"){
+    let amountAns = await inquirer.prompt(
+        [
+            {
+                name : "amount",
+                message:"enter your amount",
+                type : "number",
+            }
+        ]
+    );
+    myBalance-=amountAns.amount
+    console.log("your remaining balance is" + myBalance)
+    }else(operationAns.operation==="check balance")
+    console.log(" your balance is"+ myBalance)
+}
+else {
+    console.log("wrong pin number");
+}
+
+
+    
+
