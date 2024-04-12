084dfa76718e023a8aa6d580a1a7a422e7ce9928
zohaibmemon1515 <zohaibmemon1515@gmail.com>
2024-04-12 07:01:56 -0700

    add file

---
+++ index.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer"
+// const randomnumber = 13
+const randomnumber = Math.floor(Math.random()*7+1);
+const result = await inquirer.prompt([
+{
+  name : "userGuessednumber",
+  type : "number",
+  message : "please Guess a number 1 to 7",
+},
+
+]);
+if (result.userGuessednumber == randomnumber){
+    console.log("congratulation!You Guessed right number.")
+}else{
+    console.log("You Guessed wrong answer.")
+}
