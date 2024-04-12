e62e30f1296a77e57f29e3cc04d13c73232558d1
zohaibmemon1515 <zohaibmemon1515@gmail.com>
2024-04-12 07:07:46 -0700

    add file

---
+++ main.js
@@ -0,0 +1,5 @@
+var a = 5;
+var b = 2;
+var c;
+c = ++a + a++ + --b + b-- + a + b++ + b;
+console.log(c);
---
+++ main.ts
@@ -0,0 +1,6 @@
+// let a = 5
+// let b = 2
+// let c: number;
+// c = ++a + a++ + --b + b-- + a + b++ + b;
+// console.log(c) //22
+
